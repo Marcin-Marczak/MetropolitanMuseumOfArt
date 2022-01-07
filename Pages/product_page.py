e37f568c649a36d66b18a36041d771ecaf897575
Marcin Marczak <marczak.krakow@gmail.com>
2022-01-07 22:34:29 +0100

    Fixes in mini_cart_page and product_page files

--- Pages/product_page.py
+++ Pages/product_page.py
@@ -1,6 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 
 
 class ProductPage:
@@ -9,5 +7,4 @@ class ProductPage:
         context.product_page_add_to_cart_button = (By.ID, "product-addtocart-button")
 
     def product_page_add_product_to_cart(context):
-        WebDriverWait(context.driver, 15, 0.25).until(ec.element_to_be_clickable, context.driver.find_element(*context.product_page_add_to_cart_button))
         context.driver.find_element(*context.product_page_add_to_cart_button).click()
