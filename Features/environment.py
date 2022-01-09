ce043a6f4187766a9dec47295916ae7265fad0a3
Marcin Marczak <marczak.krakow@gmail.com>
2022-01-10 00:03:35 +0100

    Set headless mode for driver

--- Features/environment.py
+++ Features/environment.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 import json
 
 data = json.load(open("Configuration/config.json", "r"))
 
 
 def before_scenario(context, scenario):
-    context.driver = webdriver.Chrome("./chromedriver.exe")
+    chrome_options = ChromeOptions()
+    chrome_options.add_argument("--headless")
+    context.driver = webdriver.Chrome("./chromedriver.exe", options=chrome_options)
     context.driver.get(data["main_page"])
     context.driver.implicitly_wait(30)
     context.driver.set_window_size(1500, 800)
